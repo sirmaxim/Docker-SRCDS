581162ec86a71091f1d74f33978f527277c5d88d
Dane Everitt <dane@daneeveritt.com>
2016-09-28 18:50:50 -0400

    Remove old verbosity modifiers, better descriptions of events.

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 # Copyright (c) 2016 Dane Everitt <dane@daneeveritt.com>
 # MIT Licensed
 # ##
-set -x
+sleep 3
 if [ "$(pwd)" -ne "/home/container" ]; then
     cd /home/container
 fi
@@ -12,32 +12,38 @@ fi
 # Download SteamCMD, it is missing
 if [ ! -f "/home/container/steamcmd/steamcmd.sh" ]; then
     mkdir steamcmd; cd steamcmd
-    curl -sS http://media.steampowered.com/installer/steamcmd_linux.tar.gz -o steamcmd.tar.gz
+
+    set -x
+    curl -sSL -o steamcmd.tar.gz http://media.steampowered.com/installer/steamcmd_linux.tar.gz
+    set +x
+
     if [ "$?" -ne "0" ]; then
         echo "There was an error while attempting to download SteamCMD (exit code: $?)"
         exit 1
     fi
+
     tar -xzvf steamcmd.tar.gz
     rm -rf steamcmd.tar.gz
 
+    echo "Installing requested game, this could take a long time depending on game size and network."
+    set -x
     ./steamcmd.sh +login anonymous +force_install_dir /home/container +app_update $SRCDS_APPID +quit
+    set +x
 
     cd /home/container
     mkdir -p .steam/sdk32
     cp -v steamcmd/linux32/steamclient.so .steam/sdk32/steamclient.so
 else
-    echo "SteamCMD already exists on server, not downloading anything."
+    echo "Dependencies in place, to re-download this game please delete steamcmd.sh in the steamcmd directory."
 fi
 
 cd /home/container
 
 if [ -z "$STARTUP" ]; then
-    echo "No startup command was specified."
+    echo "No startup command was specified!"
     exit 1
 fi
 
 MODIFIED_STARTUP=`echo $STARTUP | perl -pe 's@{{(.*?)}}@$ENV{$1}@g'`
+echo "./srcds_run ${MODIFIED_STARTUP}"
 ./srcds_run $MODIFIED_STARTUP
-
-set +x
-exit 0
